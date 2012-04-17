42eb6ba5c589ecdd07eb100f016350bc29f8c73c
Chris Wenham <cwenham@gmail.com>
2012-04-17 18:55:12 -0400

    Implement the CompoundingsPerYear > investmentsPerYear scenario.

--- SpendingConsequences/Calculators/Investment.cs
+++ SpendingConsequences/Calculators/Investment.cs
@@ -60,7 +60,7 @@ namespace SpendingConsequences.Calculators
 			}
 		}
 		
-		public int FrequencyPerYear {
+		public int CompoundingsPerYear {
 			get {
 				switch (Compounding) {
 				case "Monthly":
@@ -69,6 +69,8 @@ namespace SpendingConsequences.Calculators
 					return 52;
 				case "Annually":
 					return 1;
+				case "Quarterly":
+					return 4;
 				default:
 					return 12;
 				}
@@ -93,45 +95,66 @@ namespace SpendingConsequences.Calculators
 			}
 		}
 		
-		#region implemented abstract members of SpendingConsequences.Calculators.ACalculator
-		public override ConsequenceResult Calculate (ConsequenceRequest request)
+		private string FormatMyCaption ()
 		{
-			if (request.InitialAmount == 0m)
-				return null;
-			
-			double compoundingPeriods = Years * FrequencyPerYear;
-			double ratePerPeriod = (double)DecimalRate / FrequencyPerYear;
-			
-			if (request.TriggerMode == TriggerType.OneTime) {
-
-				return new ConsequenceResult (this, 
-				                             request.InitialAmount * ((decimal)Math.Pow (1 + ratePerPeriod, compoundingPeriods)), 
-				                             FormatCaption (this.Caption, new Dictionary<string,string> {
-					{"Rate", DecimalRate.ToString ()},
-					{"Years", Years.ToString ()}
-				}),
-				                              this.ImageName);
-			} else {
-				// Use a mathethematical series to calculate the mutliplier
-				// Described here: http://mathdude.quickanddirtytips.com/math-dude-web-bonus.aspx
-				double seriesSum = 0;
-				for (int i = 1; i <= compoundingPeriods; i++) {
-					seriesSum += Math.Pow (1 + ratePerPeriod, i);
-				}
-				
-				// Since the contribution rate may not coincide with the compounding rate, we
-				// need to calculate the investment-per-compounding period
-				double recurringInvestment = (((double)request.InitialAmount) * InvestmentsPerYear (request.TriggerMode)) / FrequencyPerYear;
-				
-				return new ConsequenceResult (this,
-				                             (decimal)(recurringInvestment * seriesSum),
-				                             FormatCaption (this.Caption, new Dictionary<string,string> {
+			return FormatCaption (this.Caption, new Dictionary<string,string> {
 					{"Rate", Rate.ToString ()},
 					{"Years", Years.ToString ()},
 					{"Compounding", Compounding}
-				}),
-				                              this.ImageName);
+				});
+		}
+		
+		#region implemented abstract members of SpendingConsequences.Calculators.ACalculator
+		public override ConsequenceResult Calculate (ConsequenceRequest request)
+		{
+			// For some of the scenarios below we have to sacrifice a bit of accuracy in order to produce results
+			// within a few milliseconds. EG: A savings account may compound monthly but calculate interest daily, but
+			// that would require more CPU time than we can afford.
+			
+			if (request.InitialAmount == 0m)
+				return null;
+			
+			double compoundingPeriods = Years * CompoundingsPerYear;
+			double ratePerPeriod = (double)DecimalRate / CompoundingsPerYear;
+			int investmentsPerYear = InvestmentsPerYear (request.TriggerMode);
+			
+			if (request.TriggerMode == TriggerType.OneTime) {
+				// A one-time investment means we can use the compound interest formula
+
+				return new ConsequenceResult (this, 
+				                             request.InitialAmount * ((decimal)Math.Pow (1 + ratePerPeriod, compoundingPeriods)), 
+				                             FormatMyCaption (),
+				                             this.ImageName);
+			} else if (CompoundingsPerYear == investmentsPerYear) {
+				// Compounding frequency is the same as the investment frequency, so we can
+				// use a mathematical series.
+				// Described here: http://mathdude.quickanddirtytips.com/math-dude-web-bonus.aspx
+				
+				double seriesSum = 0;
+				for (int i = 1; i <= compoundingPeriods; i++)
+					seriesSum += Math.Pow (1 + ratePerPeriod, i);
+				
+				return new ConsequenceResult (this,
+				                             (decimal)(((double)request.InitialAmount) * seriesSum),
+				                             FormatMyCaption (),
+				                             this.ImageName);
+			} else if (CompoundingsPerYear > investmentsPerYear) {
+				// We need to take the sum of sub-periods and apply the standard compound interest formula to each
+				
+				int investmentPeriods = Years * investmentsPerYear;
+				int compoundingsPerInvestment = ((int)Math.Floor (((double)CompoundingsPerYear / investmentsPerYear)));
+				
+				double sum = ((double)request.InitialAmount) * Math.Pow (1 + ratePerPeriod, compoundingsPerInvestment);
+				for (int i = 1; i <= investmentPeriods - 1; i++)
+					sum = (sum + ((double)request.InitialAmount)) * Math.Pow (1 + ratePerPeriod, compoundingsPerInvestment);
+				
+				return new ConsequenceResult (this,
+				                             (decimal)sum,
+				                             FormatMyCaption (),
+				                             this.ImageName);					
 			}
+			
+			return null;
 		}
 		#endregion
 	}
