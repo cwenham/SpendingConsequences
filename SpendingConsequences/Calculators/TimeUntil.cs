21c8c344619f1b4c3a4a1492df6213b27d26b8fd
Chris Wenham <cwenham@gmail.com>
2012-04-26 18:44:50 -0400

    Remove unused code.

--- SpendingConsequences/Calculators/TimeUntil.cs
+++ SpendingConsequences/Calculators/TimeUntil.cs
@@ -35,28 +35,22 @@ namespace SpendingConsequences.Calculators
 			double givenUnitsUntil = (double)Cost / ((double)request.InitialAmount);
 			
 			TimeSpan timeUntil;
-			double divider = 0;
 			
 			switch (request.TriggerMode) {
 			case TriggerType.PerDay:
 				timeUntil = new TimeSpan (((int)Math.Ceiling (givenUnitsUntil)), 0, 0, 0);
-				divider = 1;
 				break;
 			case TriggerType.PerWeek:
 				timeUntil = new TimeSpan (((int)Math.Ceiling (givenUnitsUntil * 7)), 0, 0, 0);
-				divider = 7;
 				break;
 			case TriggerType.PerMonth:
 				timeUntil = new TimeSpan (((int)Math.Ceiling (givenUnitsUntil * 30)), 0, 0, 0);
-				divider = 30;
 				break;
 			case TriggerType.PerQuarter:
 				timeUntil = new TimeSpan (((int)Math.Ceiling (givenUnitsUntil * 90)), 0, 0, 0);
-				divider = 90;
 				break;
 			case TriggerType.PerYear:
 				timeUntil = new TimeSpan (((int)Math.Ceiling (givenUnitsUntil * 365.25)), 0, 0, 1);
-				divider = 365.25;
 				break;
 			default:
 				break;
